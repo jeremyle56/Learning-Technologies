f15ac502f1ecbc948dbbe6450b31b64024b61938
Jeremy <jeremy.le420@gmail.com>
2023-01-10 19:36:16 +1100

    Completed MongoDb Demo

--- CSESoc_Open_Dev/Express_MongoDB/routes/stock.js
+++ CSESoc_Open_Dev/Express_MongoDB/routes/stock.js
@@ -3,36 +3,30 @@ const stockRouter = express.Router()
 const db = require('../db')
 const fs = require('fs/promises')
 
-stockRouter.get('/', (req, res) => {
-    res.send('This is the stock route')
+stockRouter.get('/', async (req, res) => {
+    let items = await db.getStockCollection().find().toArray()
+    res.send(items)
 })
 
-stockRouter.get('/:name', (req, res) => {
-    for (i in db) {
-        if (db[i].name === req.params.name) {
-            res.send(db[i])
-            return;
-        }
-    }
+stockRouter.get('/:name', async (req, res) => {
+    const stockCollection = db.getStockCollection()
+    let item = await stockCollection.findOne({ name: req.params.name })
+    res.send(item);
 })
 
-stockRouter.post('/addItem', (req, res) => {
-    db.push(req.body)
-    fs.writeFile('./db.json', JSON.stringify(db))
-    res.send('Item added')
+stockRouter.post('/addItem', async (req, res) => {
+    await db.getStockCollection().insert(req.body)
+    res.send("success!")
 })
 
-stockRouter.put('/update', (req, res) => {
-    db.splice(0, db.length)
-    db.push(req.body)
-    fs.writeFile('./db.json', JSON.stringify(db))
+stockRouter.put('/update', async (req, res) => {
+    await db.getStockCollection().deleteMany({})
+    await db.getStockCollection().insert(req.body)
     res.send('Data updated')
 })
 
-stockRouter.delete('/deleteItem/:name', (req, res) => {
-    const index = db.findIndex(a => a.name === req.params.name)
-    db.splice(index, 1);
-    fs.writeFile('./db.json', JSON.stringify(db))
+stockRouter.delete('/deleteItem/:name', async (req, res) => {
+    await db.getStockCollection().deleteOne({ name: req.params.name })
     res.send('Item removed')
 })
 
